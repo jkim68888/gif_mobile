048bff43ccaa30dc8003214a15a772a39857970b
DESKTOP-1P0J4O1\user <jkim68888@gmail.com>
2021-02-14 16:31:22 +0900

    서브페이지 css 작업중

--- js/customer.js
+++ js/customer.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
     var btn = '#faqBox .tabBar > ul';
     var content = '#faqBox .content';
-    var easing = 'easeOutQuart';
     
     $(btn).find('li:first p').addClass('active');
     $(btn).find('li:first a').addClass('active');
@@ -13,8 +12,8 @@ $(document).ready(function(){
         $(btn).find('a').removeClass('active');
         $(this).addClass('active');
         var index = $(this).parent().index();
-        $(content).find('div').stop().slideUp(easing);
-        $(content).find('div').eq(index).stop().slideDown(easing);
+        $(content).find('div').stop().fadeOut(0);
+        $(content).find('div').eq(index).stop().fadeIn(0);
     });
 });
 
